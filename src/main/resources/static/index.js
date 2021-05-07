2339910b30f07b93544db608b307e633de5c6899
JohnnyGomezzz <seldesign@mail.ru>
2021-05-07 10:50:36 +0300

    lesson 10 Cart sum added

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,12 +1,13 @@
 angular.module('app', []).controller('indexController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189/market';
 
+    $scope.cartSum = 0;
+
     $scope.init = function () {
         $http.get(contextPath + '/api/v1/products')
             .then(function (response) {
                 $scope.products = response.data;
                 $scope.showCart;
-                $scope.cartSum = 0;
             });
     };
 
