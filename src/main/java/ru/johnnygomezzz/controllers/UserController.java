3fc4b2cb3f7561a89e3eab411af120bd9ec9bfa8
JohnnyGomezzz <seldesign@mail.ru>
2021-05-13 12:50:16 +0300

    lesson 12 hw in process

---
+++ src/main/java/ru/johnnygomezzz/controllers/UserController.java
@@ -0,0 +1,25 @@
+package ru.johnnygomezzz.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.johnnygomezzz.dtos.UserDto;
+import ru.johnnygomezzz.error_handling.ResourceNotFoundException;
+import ru.johnnygomezzz.models.User;
+import ru.johnnygomezzz.services.UserService;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public UserDto getCurrentUsername(Principal principal) {
+        User currentUser = userService.findByUsername(principal.getName()).orElseThrow(() -> new ResourceNotFoundException("User doesn't exist"));
+        return new UserDto(currentUser.getUsername(), currentUser.getEmail());
+    }
+}
