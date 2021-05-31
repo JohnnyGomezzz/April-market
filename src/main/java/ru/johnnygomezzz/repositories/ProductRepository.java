9dbca88054dbbbe4e18e3195f2a9629dc2b20ba9
JohnnyGomezzz <seldesign@mail.ru>
2021-05-31 16:36:49 +0300

    lesson 16 hw in process

--- src/main/java/ru/johnnygomezzz/repositories/ProductRepository.java
+++ src/main/java/ru/johnnygomezzz/repositories/ProductRepository.java
@@ -3,10 +3,16 @@ package ru.johnnygomezzz.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.johnnygomezzz.models.Product;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllBy(Pageable pageable);
+
+    @Query("select p from Products p where p.id = ?1")
+    Optional<Product> findById(Long id);
 }
