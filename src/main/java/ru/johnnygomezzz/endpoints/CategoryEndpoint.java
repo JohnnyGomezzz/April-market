bc2fe15d6e51ecfe6b2d26d961dddfc2dd429df6
JohnnyGomezzz <seldesign@mail.ru>
2021-06-02 13:18:21 +0300

    lesson 16 hw in process

--- src/main/java/ru/johnnygomezzz/endpoints/CategoryEndpoint.java
+++ src/main/java/ru/johnnygomezzz/endpoints/CategoryEndpoint.java
@@ -1,11 +1,7 @@
 package ru.johnnygomezzz.endpoints;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
-import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import ru.johnnygomezzz.services.CategoryService;
 
 @Endpoint
@@ -13,27 +9,4 @@ import ru.johnnygomezzz.services.CategoryService;
 public class CategoryEndpoint {
     private static final String NAMESPACE_URI = "http://www.johnnygomezzz.ru/categories";
     private final CategoryService categoryService;
-
-    /*
-        Пример запроса: POST http://localhost:8189/market/ws
-
-        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-          xmlns:f="http://www.johnnygomezzz.ru/groups">
-            <soapenv:Header/>
-            <soapenv:Body>
-                <f:getCategoryByTitleRequest>
-                    <f:title>Продукты питания</f:title>
-                </f:getCategoryByTitleRequest>
-            </soapenv:Body>
-        </soapenv:Envelope>
-     */
-
-//    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCategoryByTitleRequest")
-//    @ResponsePayload
-//    @Transactional
-//    public GetCategoryByTitleResponse getGroupByTitle(@RequestPayload GetCategoryByTitleRequest request) {
-//        GetCategoryByTitleResponse response = new GetCategoryByTitleResponse();
-//        response.setGroup(categoryService.getByTitle(request.getTitle()));
-//        return response;
-//    }
 }
