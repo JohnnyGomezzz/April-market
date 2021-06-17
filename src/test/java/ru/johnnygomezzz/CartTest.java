5270afc834f373fddd963598c082a31f1afbfc8c
JohnnyGomezzz <seldesign@mail.ru>
2021-06-17 23:05:20 +0300

    lesson 19 hw in process

--- src/test/java/ru/johnnygomezzz/CartTest.java
+++ src/test/java/ru/johnnygomezzz/CartTest.java
@@ -1,7 +1,6 @@
 package ru.johnnygomezzz;
 
 import ru.johnnygomezzz.models.Product;
-import ru.johnnygomezzz.test.DemoBean;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,23 +9,27 @@ import ru.johnnygomezzz.utils.Cart;
 
 import java.math.BigDecimal;
 
-//@SpringBootTest(classes = DemoBean.class)
-//public class CartTest {
-//    @Autowired
-//    private Cart cart;
-//
-//    @Test
-//    public void cartFillingTest() {
-//        for (int i = 0; i < 10; i++) {
-//            Product product = new Product();
-//            long bookId = i / 2 + 1;
-//            product.setId(bookId);
-//            product.setPrice(new BigDecimal(100 + bookId * 10));
-//            product.setTitle("Book #" + bookId);
-//            cart.addOrIncrement(book);
-//        }
-//        Assertions.assertEquals(5, cart.getItems().size());
-//        cart.clear();
-//        Assertions.assertEquals(0, cart.getItems().size());
-//    }
-//}
+@SpringBootTest(classes = Cart.class)
+public class CartTest {
+    @Autowired
+    private Cart cart;
+
+    @Test
+    public void cartFillingTest() {
+        for (int i = 0; i < 10; i++) {
+            Product product = new Product();
+            long productId = i / 2 + 1;
+            product.setId(productId);
+            product.setPrice(new BigDecimal(100 + productId * 10));
+            product.setTitle("Product #" + productId);
+            cart.addToCart(product);
+        }
+        Assertions.assertEquals(5, cart.getItems().size());
+        cart.deleteAllById(2L);
+        Assertions.assertEquals(4, cart.getItems().size());
+        cart.deleteById(1L);
+        Assertions.assertEquals(4, cart.getItems().size());
+        cart.deleteAll();
+        Assertions.assertEquals(0, cart.getItems().size());
+    }
+}
