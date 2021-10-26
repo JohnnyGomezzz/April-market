f3ae5447da43e4e148329bfe6b9e288142f722ce
JohnnyGomezzz <seldesign@mail.ru>
2021-10-26 14:51:30 +0300

    lesson 5 in process

---
+++ students/src/main/java/ru/johnnygomezzz/MainApp.java
@@ -0,0 +1,58 @@
+package ru.johnnygomezzz;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+import ru.johnnygomezzz.entity.Student;
+
+public class MainApp {
+    private static SessionFactory factory;
+
+    public static void init() {
+        factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+    }
+
+    public static void main(String[] args) {
+        try {
+            init();
+            prepareData();
+            try (Session session = factory.getCurrentSession()) {
+                session.beginTransaction();
+                Student student = session.get(Student.class, 1L);
+                System.out.println(student);
+                session.getTransaction().commit();
+            }
+        } finally {
+            shutdown();
+        }
+    }
+
+    public static void prepareData() {
+        Session session = null;
+        try {
+            String sql = Files.lines(Paths.get("db.sql")).collect(Collectors.joining(" "));
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public static void shutdown() {
+        factory.close();
+    }
+}
---
+++ students/src/main/java/ru/johnnygomezzz/entity/Student.java
@@ -0,0 +1,18 @@
+package ru.johnnygomezzz.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "mark")
+    private int mark;
+}
